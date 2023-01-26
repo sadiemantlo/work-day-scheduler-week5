8c6cd67d754abc1c4da6f99ad350ed3d27fb3107
Sadie Mantlo <sadiemantlo@Sadies-MacBook-Air.local>
2023-01-25 17:07:59 -0800

    final commit

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,8 @@
 // adding the current date to the HTML
 var today = dayjs();
 $('#currentDay').text(today.format('MMM D, YYYY'));
+// adding variable for the current hour
+var currHour = dayjs().hour();
 
 $(function () {
     // event listner for save buttons and saving to local storage
@@ -20,11 +22,13 @@ for (var i = 9; i <= 17; i++) {
   updateHour(i);
 }
 
-function updateHour() {
-  var currHour = dayjs().hour();
-  $('.time-block').each(function() {
-    var scheduledHour = parseInt($(this).attr('id').split('-')[1]);
 
+function updateHour() {
+ 
+  $('.time-block').each(function() {
+// checking the hour of each time block  
+    var scheduledHour = parseInt($(this).attr('id').split('-')[1]);
+// setting the class to match if the time block hour is in past, present or future 
   if (scheduledHour < currHour) {
     $(this).addClass('past');
   } else if (scheduledHour === currHour) {
